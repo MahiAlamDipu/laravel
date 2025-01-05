70e1f03e1b9a8c73837fbd065cc0f605e4175b73
mahi-2003099 <mahialamdipu99@gamil.com>
2025-01-05 18:50:02 +0600

    showing dynamic data

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,9 @@ Route::get('/', function () {
 Route::get('/greeting', function () {
     return view('greeting');
 });
+Route::get('/users', function () {
+    $users =['dipu','sagar','rifat'];
+    return view('users',compact('users'));
+});
+
 
